b8121ea74524cd1e258dcc1d23bda8b2f71cb1b5
Yangsh888 <yangsh888@foxmail.com>
2026-05-19 15:06:50 +0800

    Bump plugin version to 1.3.0
    Update RenewSEO/Plugin.php docblock to reflect version 1.3.0 (changed from 1.2.0), indicating a new plugin release/preparation.

--- RenewSEO/Plugin.php
+++ RenewSEO/Plugin.php
@@ -20,7 +20,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
  * @package RenewSEO
  * @author TypeRenew
  * @link https://www.typerenew.com/
- * @version 1.2.0
+ * @version 1.3.0
  * @since 1.4.1
  */
 class Plugin implements PluginInterface
