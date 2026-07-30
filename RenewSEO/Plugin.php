fd8505ab8444b9ba17ab67fca52919df368d5f06
Yangsh888 <yangsh888@foxmail.com>
2026-07-30 10:36:50 +0800

    Bump RenewSEO plugin version to 1.3.1
    Update the plugin version in RenewSEO/Plugin.php from 1.3.0 to 1.3.1 to reflect a new release/patch. No functional changes were made.

--- RenewSEO/Plugin.php
+++ RenewSEO/Plugin.php
@@ -20,7 +20,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) {
  * @package RenewSEO
  * @author TypeRenew
  * @link https://www.typerenew.com/
- * @version 1.3.0
+ * @version 1.3.1
  * @since 1.4.1
  */
 class Plugin implements PluginInterface
